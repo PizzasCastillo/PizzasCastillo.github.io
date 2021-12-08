d835650b542e4e68989f2201ee60e3af9a77ed95
PizzasCastillo <95323506+PizzasCastillo@users.noreply.github.com>
2021-12-08 10:31:39 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,21 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDAkWZ2ZBTd3PQzdv1XnSeLhMW1T7So5Q4",
-  authDomain: "pizzascastillo-ef83a.firebaseapp.com",
-  projectId: "pizzascastillo-ef83a",
-  storageBucket: "pizzascastillo-ef83a.appspot.com",
-  messagingSenderId: "678552553175",
-  appId: "1:678552553175:web:0d49a683731dde247b1292",
+firebase.initializeApp({
+  apiKey: "AIzaSyBb4rhWfiyy478hk1ulGawXZG9e5wtQd0w",
+  authDomain: "pizzascastillo-ff02c.firebaseapp.com",
+  projectId: "pizzascastillo-ff02c",
+  storageBucket: "pizzascastillo-ff02c.appspot.com",
+  messagingSenderId: "334670760409",
+  appId: "1:334670760409:web:05ae16bc1c3ef4463471c8",
   measurementId: "${config.measurementId}"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+});
